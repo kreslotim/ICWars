759cc9489b551a472733c5c420f42c6605c42ce0
annhit29 <ann20010929@gmail.com>
2021-12-07 21:56:19 +0100

    ajout methode addEdge dans Unit.java

--- java/ch/epfl/cs107/play/game/icwars/actor/Unit.java
+++ java/ch/epfl/cs107/play/game/icwars/actor/Unit.java
@@ -20,15 +20,6 @@ public class Unit extends ICWarsActor {
     private Faction faction;
     private Sprite sprite;
     private ICWarsRange range;
-    private int fromX;
-    private int fromY;
-
-//    private void edge(){
-//        for
-//            for
-//    }
-
-
 
     /**
      * Default Unit constructor
@@ -45,6 +36,36 @@ public class Unit extends ICWarsActor {
         this.radius = radius;
         this.faction = faction;
     }
+
+    /**
+     * (pdf p.12) a unit positioned in the range (fromX, fromY ), its attribute Range must
+     * be initialized when creating the unit so as to contain all the nodes that are placed in the
+     * range (x + fromX, y + fromY ), where x and y are between −radius and radius and where
+     * radius is the unit’s radius of action. Adding a node will be done using the AddNode from
+     * ICWarsRange. Attention, only the nodes which correspond to valid positions in the grid
+     * must be added. The four booleans required by the AddNode method must be true if the
+     * node corresponding to the coordinate (x + fromX, y + fromY ) has a neighbor on its left in
+     * the graph ICWarsRange (resp. above, right and below). For the left neighbor for example
+     * this will be the case if x is strictly greater than −radius and x + fromX is greater than
+     * strictly zero
+     **/
+    private void addEdge() {
+        DiscreteCoordinates from = getCurrentMainCellCoordinates();
+        for (int x = -radius; x <= radius; ++x) {
+            for (int y = -radius; y <= radius; ++y) {
+                DiscreteCoordinates thisCell = new DiscreteCoordinates(from.x + x, from.y + y);
+                if (getOwnerArea().getWidth() < 0 || getOwnerArea().getHeight() < 0 ||
+                        getOwnerArea().getWidth() > thisCell.x || getOwnerArea().getHeight() > thisCell.y) {
+                    // row < 0 || col < 0 || row >= image.length || col >= image[row].length
+                    range.addNode(thisCell, ((x > -radius) && (x + from.x > 0)), ((x > radius) &&
+                            (x + from.x < 0)), ((y > -radius) && (y + from.x > 0)), ((y > radius) && (y + from.y < 0)));
+
+                }
+
+            }
+        }
+    }
+
 
     int getDamage() {
         return damage;
